0d770942412a43935b35c062c8d2409f88271103
cheap-glitch <cheap.glitch@gmail.com>
2020-01-18 13:59:20 +0100

    Supplement tests

--- tests/imports.test.js
+++ tests/imports.test.js
@@ -6,22 +6,61 @@
 const { expect }         = require('chai');
 const generateImportCode = require('../src/imports');
 
-const generate = _options => generateImportCode({ component: 'fa', components: {}, ..._options});
-const wrapTest = _str => `
-		import Vue from 'vue';
-		import { library } from '@fortawesome/fontawesome-svg-core';
-		${_str}
-	`.replace(/\t+|  +|\n/g, '');
+/**
+ * Helper functions
+ */
+const generate     = _options => generateImportCode({ component: 'fa', components: {}, ..._options});
+const wrapExpected = _str => `
+	import Vue from 'vue';
+	import { library } from '@fortawesome/fontawesome-svg-core';
+	${_str}`
+	.replace(/\t+|  +|\n/g, '');
 
-
+/**
+ * Tests
+ */
 describe("import code generation", () => {
 
 	it("imports full sets", () => {
+		// Full name
 		expect(generate({
-			imports: ['@fortawesome/pro-solid-icons-svg'],
-		})).to.equal(wrapTest(`
+			imports: ['@fortawesome/pro-solid-svg-icons'],
+		})).to.equal(wrapExpected(`
 			import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-			import { fas } from '@fortawesome/pro-solid-icons-svg-svg-icons';
+			import { fas } from '@fortawesome/pro-solid-svg-icons';
+
+			Vue.component('fa', FontAwesomeIcon);
+			library.add(fas);
+		`));
+
+		// No prefix
+		expect(generate({
+			imports: ['pro-solid-svg-icons'],
+		})).to.equal(wrapExpected(`
+			import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+			import { fas } from '@fortawesome/pro-solid-svg-icons';
+
+			Vue.component('fa', FontAwesomeIcon);
+			library.add(fas);
+		`));
+
+		// No suffix
+		expect(generate({
+			imports: ['@fortawesome/pro-solid'],
+		})).to.equal(wrapExpected(`
+			import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+			import { fas } from '@fortawesome/pro-solid-svg-icons';
+
+			Vue.component('fa', FontAwesomeIcon);
+			library.add(fas);
+		`));
+
+		// Neither
+		expect(generate({
+			imports: ['pro-solid'],
+		})).to.equal(wrapExpected(`
+			import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+			import { fas } from '@fortawesome/pro-solid-svg-icons';
 
 			Vue.component('fa', FontAwesomeIcon);
 			library.add(fas);
